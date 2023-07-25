320950399547b99bf0fc4e3494b9893d301ab442
Vladislav_Antipov <lifyxus@gmail.com>
2023-07-25 18:11:14 +0300

    bruh, forgot

--- src/modules/database.ts
+++ src/modules/database.ts
@@ -26,34 +26,34 @@ const Server: Model<IServer> = model<IServer>('servers', ServerSchema);
 export async function run() {
     await connect(process.env.CONNECTION_STRING as string);
 
-    // setInterval(async () => {
-    //     for (const server of getServers()) {
-    //         const document = await Server.findById(server.serverId);
-    //         const record: IRecord = {
-    //             players: parseInt(server.players.split('/')[0]),
-    //             time: Date.now(),
-    //         };
-    //
-    //         if (document) {
-    //             document.records.push(record);
-    //             const update = {
-    //                 $set: {
-    //                     records: document.records,
-    //                 },
-    //             }
-    //
-    //             await Server.findOneAndUpdate({ _id: server.serverId }, update);
-    //         } else {
-    //             const object = new Server({
-    //                 _id: server.serverId,
-    //                 isoCode: server.isoCode,
-    //                 records: [record],
-    //             });
-    //
-    //             await object.save();
-    //         }
-    //     }
-    // }, 40000);
+    setInterval(async () => {
+        for (const server of getServers()) {
+            const document = await Server.findById(server.serverId);
+            const record: IRecord = {
+                players: parseInt(server.players.split('/')[0]),
+                time: Date.now(),
+            };
+
+            if (document) {
+                document.records.push(record);
+                const update = {
+                    $set: {
+                        records: document.records,
+                    },
+                }
+
+                await Server.findOneAndUpdate({ _id: server.serverId }, update);
+            } else {
+                const object = new Server({
+                    _id: server.serverId,
+                    isoCode: server.isoCode,
+                    records: [record],
+                });
+
+                await object.save();
+            }
+        }
+    }, 40000);
 }
 
 export async function getDocumentById(id: number) {
